708b267ca83711c5b85b43340bc54ee4dc8dc351
Vinnie Moscaritolo <vinnie@vmeng.com>
2021-12-19 15:48:20 -0800

    still fighting sourcetreee

---
+++ coopserver/src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  coopserver
+//
+//  Created by Vincent Moscaritolo on 12/19/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
