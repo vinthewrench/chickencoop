e88950dae76ae2307b3571d97e688ac109bd945c
Vinnie Moscaritolo <vinnie@vmeng.com>
2021-12-19 16:31:31 -0800

    coopserver initial

--- coopserver/src/main.cpp
+++ coopserver/src/main.cpp
@@ -6,9 +6,52 @@
 //
 
 #include <iostream>
+ 
+#include "LogMgr.hpp"
+
+#include "TCPServer.hpp"
+#include "Telnet/TelnetServerConnection.hpp"
+#include "REST/RESTServerConnection.hpp"
+#include "ServerCmdQueue.hpp"
+#include "Telnet/CmdLineRegistry.hpp"
+
+ 
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+
+	int telnetPort = 2020 ; // pumphouse.getDB()->getTelnetPort();
+	int restPort =  8080 ; // pumphouse.getDB()->getRESTPort();
+	bool remoteTelnet = true ;// pumphouse.getDB()->getAllowRemoteTelnet();
+
+	 
+	// create the server command processor
+	auto cmdQueue = new ServerCmdQueue(NULL);
+//	registerServerNouns();
+//	registerCommandsLineFunctions();
+	
+	
+	TCPServer telnet_server(cmdQueue);
+	telnet_server.begin(telnetPort, true, [=](){
+		return new TelnetServerConnection();
+	});
+
+	TCPServer rest_server(cmdQueue);
+	rest_server.begin(restPort, remoteTelnet, [=](){
+		return new RESTServerConnection();
+	});
+
+	
+	 
+	// run the main loop.
+	while(true) {
+//
+//		pumphouse.setActiveConnections( rest_server.hasActiveConnections()
+//												|| telnet_server.hasActiveConnections());
+ 	sleep(2);
+	}
+
 	return 0;
+
+
+
 }
